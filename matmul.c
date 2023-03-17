9f2dc1b86c32cf725ed20175f79bb284c6782986
wuliJerry <wuliJerry927@outlook.com>
2023-03-17 17:53:53 +0800

    experiment 1

--- matmul.c
+++ matmul.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <likwid.h>
+#include <omp.h>
 
 void matmul(int N, float *A, float *B, float *C, int *tileSizes) {
     LIKWID_MARKER_START("matrix_multiplication");
+    int num_tiles_i = (N + tileSizes[0] - 1) / tileSizes[0];
+    int num_tiles_j = (N + tileSizes[1] - 1) / tileSizes[1];
+    int num_tiles_k = (N + tileSizes[2] - 1) / tileSizes[2];
+    int total_tiles = num_tiles_i * num_tiles_j * num_tiles_k;
+    int tile_counter = 0;
+
+	#pragma omp parallel for schedule(static) collapse(2)
     for (int i = 0; i < N; i += tileSizes[0]) {
         for (int j = 0; j < N; j += tileSizes[1]) {
             for (int k = 0; k < N; k += tileSizes[2]) {
@@ -19,9 +27,13 @@ void matmul(int N, float *A, float *B, float *C, int *tileSizes) {
                         C[ii * N + jj] += c;
                     }
                 }
+                tile_counter++;
+                 //printf("\rProgress: %d/%d (%d%%)", tile_counter, total_tiles, tile_counter * 100 / total_tiles);
+                 //fflush(stdout);
             }
         }
     }
+    printf("\n");
     LIKWID_MARKER_STOP("matrix_multiplication");
 }
 
@@ -32,7 +44,7 @@ int main(int argc, char **argv) {
     int tileSizeI = atoi(argv[2]);
     int tileSizeJ = atoi(argv[3]);
     int tileSizeK = atoi(argv[4]);
-	int tileSize[3] = {tileSizeI, tileSizeJ, tileSizeK};
+    int tileSize[3] = {tileSizeI, tileSizeJ, tileSizeK};
 
     float *A = (float*)malloc(N * N * sizeof(float));
     float *B = (float*)malloc(N * N * sizeof(float));
--- run.sh
+++ run.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
 # Compile the matrix multiplication code with LIKWID_PERFMON flag
-gcc -O3 -DLIKWID_PERFMON -llikwid matmul.c -o matmul
+gcc -O3 -DLIKWID_PERFMON -fopenmp -llikwid matmul.c -o matmul
 
 # Loop over different tile sizes
 mkdir output
 
-for ((i=32; i<=128; i+=32)); do
-    for ((j=32; j<=128; j+=32)); do
-        for ((k=32; k<=128; k+=32)); do
+export OMP_NUM_THREADS=14
+
+for ((i=256; i<=1024; i+=256)); do
+    for ((j=256; j<=1024; j+=256)); do
+        for ((k=256; k<=1024; k+=256)); do
             # Run the matrix multiplication code with transformed loops and measure L1 data movements
-            likwid-perfctr -C 0-3 -g L2 -m ./matmul 1024 "$i" "$j" "$k" > output/output_${i}_${j}_${k}.txt
+            likwid-perfctr -C 0-13 -g L2 -m ./matmul 8192 "$i" "$j" "$k" > output/output_${i}_${j}_${k}.txt
         done
     done
 done
