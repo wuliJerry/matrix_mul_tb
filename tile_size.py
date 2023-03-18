d60945528be3f732ad78e29d6bbe0b309acf48ab
yuzeng <yuzeng@umich.edu>
2023-03-17 21:33:44 -0400

    add parameters to the python script

--- tile_size.py
+++ tile_size.py
@@ -1,9 +1,10 @@
-factors = [factor for factor in range(1, 8193) if 8192 % factor == 0]
+matrix_size = 8192
+cache_size = 512*1024
+factors = [factor for factor in range(1, matrix_size+1) if matrix_size % factor == 0]
 
 for i in factors:
     for j in factors:
-        if i*j <= 512*1024:
+        if i*j <= cache_size:
             for k in factors:
-                if i*k + j*k + i*j <= 512*1024 and 2*i*k + 2*j*k + i*j >= 512*1024:
+                if i*k + j*k + i*j <= cache_size and 2*i*k + 2*j*k + i*j >= cache_size:
                     print(f"{i}, {j}, {k}")
-
